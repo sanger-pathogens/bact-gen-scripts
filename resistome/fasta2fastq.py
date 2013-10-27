a32d408fa7987854ba16974ca6af01442643e49f
Pathogen Genomics, Wellcome Trust Sanger Institute <psu-dev@sanger.ac.uk>
2013-10-27 23:00:29 +0000

    Daily update of sh16 scripts

---
+++ resistome/fasta2fastq.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+
+import string
+import os, sys
+from optparse import OptionParser
+from Bio import SeqIO
+
+##########################################
+# Function to Get command line arguments #
+##########################################
+
+
+def main():
+
+	usage = "usage: %prog [options] <list of mfa files>"
+	parser = OptionParser(usage=usage)
+
+	parser.add_option("-f", "--fastq", action="store", dest="fastq", help="Input fastq file", default="")
+	#Could add more options in here so people can specify similarities etc.
+	
+	
+	return parser.parse_args()
+
+
+################
+# Main program #
+################		
+
+if __name__ == "__main__":
+
+	#Get command line arguments
+
+	(options, args) = main()
+	
+	if options.fastq=="":
+		DoError("Fastq file (-f) required")
+	
+	if not os.path.isfile(options.fastq):
+		DoError("Cannot find file "+options.fastq)
+	
+	
+
+	
+	
+	count=SeqIO.convert(options.fastq, "fastq", "test.fasta", "fasta")
+
+	print "converted", count, "reads"
