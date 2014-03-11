d690b4e494ba6ba658039a58514515749e300ef4
Pathogen Genomics, Wellcome Trust Sanger Institute <psu-dev@sanger.ac.uk>
2014-03-11 23:00:39 +0000

    Daily update of sh16 scripts

--- iCANDY_tangle.py
+++ iCANDY_tangle.py
@@ -626,6 +626,66 @@ def read_dendropy_tree(treefile):
 		dendropy.dataio.nexustokenizer.NHX_COMMENT_FIELD_PATTERN = re.compile(r'(.+?)=({.+?}|.+?)(,|$)')
 		dendropy.dataio.nexustokenizer.FIGTREE_COMMENT_FIELD_PATTERN = re.compile(r'(.+?)=({.+?}|.+?)(,|$)')
 		
+		#And to make it reroot properly if the tree is already rooted on the midpoint
+		def reroot_at_midpoint(self, update_splits=False, delete_outdegree_one=True):
+			"""
+			Reroots the tree at the the mid-point of the longest distance between
+			two taxa in a tree.
+			Sets the rooted flag on the tree to True.
+			If `update_splits` is True, then the edges' `split_bitmask` and the tree's
+			`split_edges` attributes will be updated.
+			If the *old* root of the tree had an outdegree of 2, then after this
+			operation, it will have an outdegree of one. In this case, unless
+			`delete_outdegree_one` is False, then it will be
+			removed from the tree.
+			"""
+			from dendropy import treecalc
+			pdm = treecalc.PatristicDistanceMatrix(self)
+			n1, n2 = pdm.max_dist_nodes
+			plen = float(pdm.max_dist) / 2
+			mrca_node = pdm.mrca(n1.taxon, n2.taxon)
+	        #assert mrca_node is self.mrca(taxa=[n1.taxon, n2.taxon])
+	        #mrca_node = self.mrca(taxa=[n1.taxon, n2.taxon])
+			cur_node = n1
+	
+			break_on_node = None # populated *iff* midpoint is exactly at an existing node
+			target_edge = None
+			head_node_edge_len = None
+	
+	        # going up ...
+			while cur_node is not mrca_node:
+				if cur_node.edge.length > plen:
+					target_edge = cur_node.edge
+					head_node_edge_len = plen #cur_node.edge.length - plen
+					plen = 0
+					break
+				elif cur_node.edge.length < plen:
+					plen -= cur_node.edge.length
+					cur_node = cur_node.parent_node
+				else:
+					break_on_node = cur_node
+					break         #when find the  midpoint, it should break the loop
+	
+			assert break_on_node is not None or target_edge is not None
+	
+			if break_on_node:
+				self.reseed_at(break_on_node, update_splits=False, delete_outdegree_one=delete_outdegree_one)
+				new_seed_node = break_on_node
+			else:
+				tail_node_edge_len = target_edge.length - head_node_edge_len
+				old_head_node = target_edge.head_node
+				old_tail_node = target_edge.tail_node
+				old_tail_node.remove_child(old_head_node)
+				new_seed_node = Node()
+				new_seed_node.add_child(old_head_node, edge_length=head_node_edge_len)
+				old_tail_node.add_child(new_seed_node, edge_length=tail_node_edge_len)
+				self.reseed_at(new_seed_node, update_splits=False, delete_outdegree_one=delete_outdegree_one)
+			self.is_rooted = True
+			if update_splits:
+				self.update_splits(delete_outdegree_one=False)
+			return self.seed_node
+		
+		dendropy.dataobject.Tree.reroot_at_midpoint=reroot_at_midpoint
 		
 		def _parse_taxlabels_statement(self, taxon_set=None):
 			"""
