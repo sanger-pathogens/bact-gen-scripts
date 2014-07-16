370bbaf866d41ab0e9edc9ef92e63a86e0840d70
Pathogen Genomics, Wellcome Trust Sanger Institute <psu-dev@sanger.ac.uk>
2014-07-16 23:00:32 +0100

    Daily update of sh16 scripts

--- summarise_kraken_reports.py
+++ summarise_kraken_reports.py
@@ -29,8 +29,10 @@ def main():
 	parser = OptionParser(usage=usage)
 	
 	parser.add_option("-l", "--level", action="store", dest="level", choices=["D", "K", "P", "C", "O", "F", "G", "S", "T"], help="Taxonomic level to output. Choose from D (Domain), K (Kingdom), P (Phylum), C (Class), O (Order), F (Family), G (Genus), S (Species), T (Strain)", default="S", metavar="LEVEL", type="choice")
-	parser.add_option("-c", "--cutoff", action="store", dest="cutoff", help="Cutoff minimum % match in at least one report to include in output", default=0.0, metavar="FLOAT", type="float")
-	parser.add_option("-t", "--transpose", action="store_true", dest="transpose", help="Transpose output to have files in rows and matches in columns. Defautl is to have matches in rows and files in columns", default=False, metavar="BOOL")
+	parser.add_option("-c", "--cutoff", action="store", dest="cutoff", help="Cutoff minimum value in at least one report to include in output.", default=0.0, metavar="FLOAT", type="float")
+	parser.add_option("-t", "--transpose", action="store_true", dest="transpose", help="Transpose output to have files in rows and matches in columns. Default is to have matches in rows and files in columns", default=False, metavar="BOOL")
+	parser.add_option("-s", "--scale", action="store_true", dest="scale", help="Scale output values by the total percentage of unique matches at the chosen level for each file", default=False, metavar="BOOL")
+	parser.add_option("-v", "--value", action="store", dest="value", choices=['1', '2', '3'], help="Value to output. 1 = Percentage of reads covered by the clade rooted at this taxon, 2 = Number of reads covered by the clade rooted at this taxon, 3 = Number of reads assigned directly to this taxon", default='1', metavar="VALUE", type="choice")
 
 	return parser.parse_args()
 
@@ -45,10 +47,12 @@ def check_input_validity(options, args):
 #	if options.processors>cpu_count():
 #		DoError('You do not have '+str(options.processors)+' processors')
 #
+	
 	if options.cutoff<0:
-		DoError('Cutoff % match must be 0 or greater')
-	elif options.cutoff>=100:
-		DoError('Cutoff % match must be below 100')
+		DoError('Cutoff value must be 0 or greater')
+	if options.value=="1":
+		if options.cutoff>100:
+			DoError('Cutoff value must be 100 or below when percentage of unique matches is chosen as the value')
 		
 	for arg in args:
 		if not os.path.isfile(arg):
@@ -62,6 +66,25 @@ def check_input_validity(options, args):
 Match_type={'D':'Domain', 'K':'Kingdom', 'P':'Phylum', 'C':'Class', 'O':'Order', 'F':'Family', 'G':'Genus', 'S':'Species', 'T':'Strain'}
 
 
+
+##############################################################################
+# Print output to stdout, but catch IOErrors that happen if you pipe to head #
+##############################################################################
+
+def print_list_to_stdout(my_list):
+	try:
+		sys.stdout.write('\t'.join(map(str,my_list))+"\n")
+	except IOError:
+		try:
+			sys.stdout.close()
+		except IOError:
+			pass
+		try:
+			sys.stderr.close()
+		except IOError:
+			pass
+
+    
 
 ################
 # Main program #
@@ -77,6 +100,8 @@ if __name__ == "__main__":
 	
 	values={}
 	all_values={}
+	totals={}
+	total_assigned={}
 	UC={}
 	
 	files=args
@@ -85,6 +110,8 @@ if __name__ == "__main__":
 	for file in files:
 		values[file]={}
 		UC[file]={}
+		totals[file]=0.0
+		total_assigned[file]=0
 		
 		species_level=float("Inf")
 		fh=open(file, "rU")
@@ -92,22 +119,31 @@ if __name__ == "__main__":
 			line=line.strip()
 			words=line.split("\t")
 			name=words[5].strip()
-			PC=float(words[0])
+			if int(options.value)>1:
+				PC=int(words[int(options.value)-1])
+			else:
+				PC=float(words[int(options.value)-1])
 			
 			if options.level=="T" and words[3]=="-" and (len(words[5]) - len(words[5].lstrip(' ')))==species_level+2:
 				values[file][name]=PC
 				if not name in all_values:
 					all_values[name]=[]
 				all_values[name].append(PC)
-			if options.level=="T" and words[3]=="S":
+				totals[file]+=PC
+				total_assigned[file]+=int(words[1])
+			elif options.level=="T" and words[3]=="S":
 				species_level=(len(words[5]) - len(words[5].lstrip(' ')))
 			elif words[3]==options.level:
 				values[file][name]=PC
 				if not name in all_values:
 					all_values[name]=[]
 				all_values[name].append(PC)
+				totals[file]+=PC
+				total_assigned[file]+=int(words[1])
 			elif words[3]=="U":
 				UC[file]=PC
+				totals[file]+=PC
+				total_assigned[file]+=int(words[1])
 		fh.close()
 	
 	
@@ -119,34 +155,42 @@ if __name__ == "__main__":
 	
 	vl.sort()
 	vl.reverse()
-		
-	
 	
 	if options.transpose:
 		
-		headers=[Match_type[options.level], "Unclassified"]
+		headers=[Match_type[options.level], "Total", "Unclassified"]
 		
 		for v in vl:
 			if max(all_values[v[1]])>=options.cutoff:
 				headers.append(v[1])
-		print '\t'.join(map(str,headers))
+		print_list_to_stdout(headers)
+#		sys.stdout.write('\t'.join(map(str,headers))+"\n")
 		
 		for file in files:
 			outvalues=[file]
-			if file in UC:
-				outvalues.append(UC[file])
+			
+			if file in total_assigned:
+				outvalues.append(total_assigned[file])
 			else:
-				outvalues.append(0.0)
+				outvalues.append(0)
+			
+			if options.scale:
+				scale_factor=(1.0/totals[file])*100
+			else:
+				scale_factor=1
+			if file in UC:
+				outvalues.append(scale_factor*UC[file])
+			else:
+				outvalues.append(0)
 			for v in vl:
 			
 				if max(all_values[v[1]])>=options.cutoff:
-					
 					if v[1] in values[file]:
-						outvalues.append(values[file][v[1]])
+						outvalues.append(scale_factor*values[file][v[1]])
 					else:
-						outvalues.append(0.0)
-					
-			print '\t'.join(map(str,outvalues))
+						outvalues.append(0)
+			print_list_to_stdout(outvalues)		
+#			sys.stdout.write('\t'.join(map(str,outvalues))+"\n")
 	
 	else:
 	
@@ -154,31 +198,46 @@ if __name__ == "__main__":
 		
 		for file in files:
 			headers.append(file)
-		print '\t'.join(map(str,headers))
-	
+		print_list_to_stdout(headers)
+		#sys.stdout.write('\t'.join(map(str,headers))+"\n")
+		
+		outvalues=["Total"]
+		for file in files:
+			if file in total_assigned:
+				outvalues.append(total_assigned[file])
+			else:
+				outvalues.append(0)
+		print_list_to_stdout(outvalues)	
+#		sys.stdout.write('\t'.join(map(str,outvalues))+"\n")
+		
 		outvalues=["Unclassified"]
 		for file in files:
 			if file in UC:
 				outvalues.append(UC[file])
 			else:
-				outvalues.append(0.0)
-		print '\t'.join(map(str,outvalues))
+				outvalues.append(0)
+		print_list_to_stdout(outvalues)	
+#		sys.stdout.write('\t'.join(map(str,outvalues))+"\n")
 	
 		for v in vl:
 			
 			if max(all_values[v[1]])>=options.cutoff:
 				outvalues=[v[1]]
 				for file in files:
+					if options.scale:
+						scale_factor=(1.0/totals[file])*100
+					else:
+						scale_factor=1
 					if file in UC:
-						outvalues.append(UC[file])
+						outvalues.append(scale_factor*UC[file])
 					else:
-						outvalues.append(0.0)
+						outvalues.append(0)
 					if v[1] in values[file]:
-						outvalues.append(values[file][v[1]])
+						outvalues.append(scale_factor*values[file][v[1]])
 					else:
-						outvalues.append(0.0)
-					
-				print '\t'.join(map(str,outvalues))
+						outvalues.append(0)
+				print_list_to_stdout(outvalues)		
+#				sys.stdout.write('\t'.join(map(str,outvalues))+"\n")
 	
 	
 	
