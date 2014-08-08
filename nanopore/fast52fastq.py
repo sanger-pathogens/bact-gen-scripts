746c7c2ce0c8a7510ba724c909ab77dce179909c
Pathogen Genomics, Wellcome Trust Sanger Institute <psu-dev@sanger.ac.uk>
2014-08-08 23:00:44 +0100

    Daily update of sh16 scripts

--- nanopore/fast52fastq.py
+++ nanopore/fast52fastq.py
@@ -21,6 +21,7 @@ def main():
 		parser.add_option("-o", "--output", action="store", dest="fastq", help="output fastq file name", type="string", metavar="fastq file", default="")
 		parser.add_option("-d", "--2Ddataset", action="store", dest="twoDdataset", help="2D dataset path within fast5 file [default = %default]", metavar="path", default="/Analyses/Basecall_2D_000/BaseCalled_2D/Fastq")
 		parser.add_option("-D", "--1Ddataset", action="store", dest="oneDdataset", help="1D dataset path within fast5 file [default = %default]", metavar="path", default="/Analyses/Basecall_1D_000/BaseCalled_template/Fastq")
+		parser.add_option("-t", "--trim", action="store", dest="trim", help="Bases to trim from each end of read [default = %default]", metavar="trim", default=0, type="int")
         
 		return parser.parse_args()
 
@@ -41,12 +42,18 @@ if __name__ == "__main__":
 		print "Error: No name provided for output fastq file"
 		sys.exit()
 	
+	if options.trim<0:
+		print "Error: Trim length must be 0 or greater"
+		sys.exit()
+	
 	output=open(options.fastq, "w")
 	print "Reading fast5 files"
 	nods2D=0
+	nodstemplate2D=0
 	nods1D=0
 	fnf=0
 	utr=0
+	tooshort=0
 	success=0
 	
 	fast5s=[]
@@ -62,6 +69,7 @@ if __name__ == "__main__":
 			fnf+=1
 	
 	for fast5 in fast5s:
+		t=""
 		if not os.path.isfile(fast5):
 			fnf+=1
 			continue
@@ -74,34 +82,63 @@ if __name__ == "__main__":
 				hdf.close()
 				hopen=False
 			continue
+#		try:
+#			print hdf["Analyses/Basecall_2D_000/BaseCalled_template/Fastq"]
+#		except KeyError:
+#			continue
+#		continue
+		
 		try:
 			fq = hdf[options.twoDdataset][()]
+			t="_BaseCalled_2D"
 		except StandardError:
 			#print "Error: Cannot find dataset:", options.dataset, "within fast5 file:", fast5
 			#print "Skipping"
 			nods2D+=1
-		
+			
 			try:
-				fq = hdf[options.oneDdataset][()]
+				fq = hdf["Analyses/Basecall_2D_000/BaseCalled_template/Fastq"][()]
+				t="_BaseCalled_template"
 			except StandardError:
 				#print "Error: Cannot find dataset:", options.dataset, "within fast5 file:", fast5
 				#print "Skipping"
-				nods1D+=1
-				if hopen:
-					hdf.close()
-					hopen=False
-				continue
+				nodstemplate2D+=1
+			
+				try:
+					fq = hdf[options.oneDdataset][()]
+					t="_BaseCalled_1D"
+				except StandardError:
+					#print "Error: Cannot find dataset:", options.dataset, "within fast5 file:", fast5
+					#print "Skipping"
+					nods1D+=1
+					if hopen:
+						hdf.close()
+						hopen=False
+					continue
 		if hopen:
 			hdf.close()
 			hopen=False
 		#print "Fast5 file", options.fast5, "successfully read"
 		
+		fqstring=fq.data.__str__()
+		fqlines=fqstring.split("\n")
+		if len(fqlines[1].strip())>options.trim*2:
+			print >> output, fqlines[0].strip()+t
+			print >> output, fqlines[1].strip()[options.trim:len(fqlines[1].strip())-options.trim]
+			print >> output, fqlines[2].strip()
+			print >> output, fqlines[3].strip()[options.trim:len(fqlines[3].strip())-options.trim]
+		else:
+			tooshort+=1
+			continue
 		success+=1
-		print >> output, fq
+		
 	print fnf, "files not found"
 	print utr, "files were unreadable"
 	print nods2D, "files did not contain 2D basecalling"
+	print nodstemplate2D, "2D files did not contain template basecalling"
 	print nods1D, "files did not contain 1D basecalling"
+	if options.trim>0:
+		print tooshort, "reads too short to trim by", options.trim, "bases"
 	print success, "reads  written to", options.fastq
 	print "Done."
 	output.close()
