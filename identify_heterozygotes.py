d00d11842e8f5982b0f3669e0d168f1c6a2420e6
Pathogen Genomics, Wellcome Trust Sanger Institute <psu-dev@sanger.ac.uk>
2016-06-08 23:00:38 +0100

    Daily update of sh16 scripts

--- identify_heterozygotes.py
+++ identify_heterozygotes.py
@@ -28,10 +28,14 @@ def main():
 	usage = "usage: %prog [options] args"
 	parser = OptionParser(usage=usage)
 
-	parser.add_option("-a", "--alignment", action="store", dest="alignment", help="alignment file name. For the tab output to fit you need to give the entire alignment, not just SNP sites, which is faster.", default="", metavar="FILE")
+	parser.add_option("-v", "--vcf", action="store", dest="vcf", help="SNP_sites output vcf file name.", default="", metavar="FILE")
+	parser.add_option("-b", "--bcf", action="store", dest="bcf", help="bcf file name of test isolate.", default="", metavar="FILE")
 	parser.add_option("-t", "--tree", action="store", dest="tree", help="tree file name", default="", metavar="FILE")
-	parser.add_option("-p", "--proportion", action="store", dest="proportion", help="maximum proportion of Ns to allow in a column for it to be included in the test (i.e. ignore any sites with > than this proportion of Ns) [default=%default]", default=0.05)
-	parser.add_option("-r", "--reference", action="store", dest="reference", help="Name of reference in alignment", default="")
+	parser.add_option("-i", "--id", action="store", dest="id", help="Name of test isolate.", default="", metavar="STRING")
+	parser.add_option("-n", "--Nproportion", action="store", dest="Nproportion", help="maximum proportion of Ns to allow in isolates at a site for it to be included in the test (i.e. ignore any sites with > than this proportion of Ns) [default=%default]", default=0.05, type="float")
+	parser.add_option("-p", "--proportion", action="store", dest="proportion", help="minimum proportion of mapped reads to allow to include an allele in the test [default=%default]", default=0.05, type="float")
+	parser.add_option("-c", "--count", action="store", dest="count", help="minimum number of mapped reads to allow to include an allele in the test [default=%default]", default=5, type="int")
+	parser.add_option("-V", "--verbose", action="store_true", dest="verbose", help="Be verbose", default=False)
 	
 	return parser.parse_args()
 
@@ -45,18 +49,30 @@ def check_input_validity(options, args):
 
 #	if options.embl!="" and options.reference=='':
 #		DoError('No reference selected! If you give an embl file you must specify which sequence it is linked to (i.e. the reference)')
-	if options.alignment=='':
-		DoError('No alignment file selected!')
-	elif not os.path.isfile(options.alignment):
-		DoError('Cannot find file '+options.alignment+'!')
+	if options.vcf=='':
+		DoError('No vcf file selected!')
+	elif not os.path.isfile(options.vcf):
+		DoError('Cannot find file '+options.vcf+'!')
 	if options.tree=='':
 		DoError('No tree file selected!')
 	elif not os.path.isfile(options.tree):
 		DoError('Cannot find file '+options.tree+'!')
-	if options.proportion>1 or options.proportion<0:
+	if options.Nproportion>1 or options.Nproportion<0:
 		DoError('Maximum proportion of Ns must be between 0 and 1!')
-	if options.reference=="":
-		DoError('No reference selected!')
+	if options.proportion>1 or options.proportion<0:
+		DoError('Minimum proportion of mapped reads must be between 0 and 1!')
+	if options.count<0:
+		DoError('Minimum number of mapped reads must be greater than or equal to 0!')
+	
+	if options.bcf=="":
+		DoError('No bcf file selected!')
+	
+	if not os.path.isfile(options.bcf):
+		DoError('Cannot find file '+arg+'!')
+	
+	if options.id=="":
+		DoError("No taxon name provided")
+		
 
 gap_and_missing=set(["-", "N", "?"])
 missing_set=([ "N", "?"])
@@ -100,7 +116,176 @@ def chisquarepvalue(taxon,taxonb,taxonc):
 #	print p.right_tail
 	return p.right_tail	, diffNpositions
 
+def parse_snp_sites_vcf(handle):
+	#Read the vcf file
+	if options.verbose:
+		print "Reading snp_sites vcf file"
+	headings=[]
+	data={}
+	taxon_ids={}
+	taxa=[]
+	poscount=0
+	percent_id={}
+	for line in open(handle, "rU"):
+		line=line.strip()
+		if len(line)<2:
+			continue
+		elif line[0:2]=="##":
+			words=line[2:].split("=")
+			if words[0]=="contig":
+				data[words[2].split(",")[0]]={}
+		elif line[0]=="#":
+			headings=line.split()
+			for i, name in enumerate(headings[9:]):
+				taxon_ids[i]=name
+				taxa.append(name)
+				percent_id[i]={}
+				for j, nameb in enumerate(headings[9+i+1:]):
+					percent_id[i][i+j]=0.0
+		else:
+			words=line.split()
+			
+			CHROM=words[0]
+			POS=int(words[1])
+#			if POS>10000:
+#				break
+			if not CHROM in data:
+				DoError("CHROM ("+CHROM+") missing from data dictionary")
+			data[CHROM][POS]={}
+			ref_alleles=words[3].split(",")
+			alt_alleles=words[4].split(",")
+			alleles=ref_alleles+alt_alleles
+			poscount+=1
+			for allele in alleles:
+				data[CHROM][POS][allele]=[]
+			
+			for i, isolate in enumerate(words[9:]):
+				data[CHROM][POS][alleles[int(isolate)]].append(i)
+				for j, isolateb in enumerate(words[9+i+1:]):
+					if alleles[int(isolate)]==alleles[int(isolateb)] and alleles[int(isolate)] not in [".", "*"]:
+						percent_id[i][i+j]+=1
+			
+	
+	
+	for i in percent_id:
+		for j in percent_id[i]:
+			percent_id[i][j]=(percent_id[i][j]/poscount)*100
+			if not j in percent_id:
+				percent_id[j]={}
+			percent_id[j][i]=(percent_id[i][j]/poscount)*100
+			
+	if options.verbose:
+		print "Found "+str(poscount)+" sites"
+	return taxa, taxon_ids, data, percent_id
+	
 
+def remove_dodgy_sites(data):
+	#"Remove sites with more than the specified proportion of Ns"
+	if options.verbose:
+		print ("Removing sites with a proportion of Ns greater than "+str(options.Nproportion))
+	toremove=[]
+	for chromosome in data:
+		for position in data[chromosome]:
+			total_taxa=0.0
+			Ns=0.0
+			for allele in data[chromosome][position]:
+				total_taxa+=len(data[chromosome][position][allele])
+			if "*" in data[chromosome][position]:
+				Ns=float(len(data[chromosome][position]["*"]))
+			N_proportion=Ns/total_taxa
+			if N_proportion>options.Nproportion:
+				toremove.append([chromosome, position])
+	
+	for chromosome, position in toremove:
+		del data[chromosome][position]
+	
+	if options.verbose:
+		print "Removed "+str(len(toremove))+" sites"
+	
+	return data
+
+
+def open_bcf(filename):
+	if options.verbose:
+		print "Trying to open bcf file"
+	fileopen=False
+#	try:
+#		bcffile=open(filename, "rU")
+#		fileopen=True
+#	except StandardError:
+#		print "Cannot open bcf file as vcf"
+#	if not fileopen:
+#		try:
+#			bcffile=os.popen("bcftools view "+filename)
+#			fileopen=True
+#		except StandardError:
+#			print "Cannot open bcf file with default bcftools"
+#	if not fileopen:
+	try:
+		bcffile=os.popen("bcftools-1.2 view "+filename)
+		fileopen=True
+	except StandardError:
+		if options.verbose:
+			print "Cannot open bcf file with bcftools-1.2"
+	if not fileopen:
+		DoError("Failed to open bcf file "+filename)
+	
+	return bcffile
+
+def parse_bcf(handle, vcf_data):
+	if options.verbose:
+		print "Parsing bcf file"
+	data={}
+	for line in handle:
+		line=line.strip()
+		if len(line)<2:
+			continue
+		elif line[0:2]=="##":
+			words=line[2:].split("=")
+			if words[0]=="contig":
+				data[words[2].split(",")[0]]=[]
+		elif line[0]=="#":
+			continue
+		else:
+			words=line.split()
+			CHROM=words[0]
+			if CHROM in vcf_data:
+				POS=int(words[1])
+				if POS in vcf_data[CHROM]:
+					REF=words[3]
+					ALT=words[4]
+					if len(REF)>1 or len(ALT)>1:
+						continue
+					INFO=words[7].split(";")
+					if "INDEL" in INFO:
+						#print "skipping INDEL", POS
+						continue
+					founddp4=False
+					for i in INFO:
+						var=i.split("=")[0]
+						val=i.split("=")[1]
+						if var=="DP4":
+							dp4vals=map(int,val.split(","))
+							REFcount=dp4vals[0]+dp4vals[1]
+							ALTcount=dp4vals[2]+dp4vals[3]
+							founddp4=True
+					if founddp4 and REFcount+ALTcount>0:
+						if REFcount>=ALTcount:
+							maj_allele=REF
+							maj_allele_count=REFcount
+							min_allele=ALT
+							min_allele_count=ALTcount
+						else:
+							min_allele=REF
+							min_allele_count=REFcount
+							maj_allele=ALT
+							maj_allele_count=ALTcount
+						data[CHROM].append([POS, maj_allele, min_allele, maj_allele_count, min_allele_count])
+	
+	return data
+					
+			
+	
 
 
 ################
@@ -120,170 +305,100 @@ if __name__ == "__main__":
 	check_input_validity(options, args)
 	
 	
-	#Read the alignment file
+	#Read the vcf file
+	
+	taxon_list, taxon_id_dictionary, vcf_data, percent_id=parse_snp_sites_vcf(options.vcf)
+	tid=-1
+	for t, taxon in enumerate(taxon_list):
+		if taxon==options.id:
+			tid=t
+	if tid==-1:
+		DoError("Specified name not in vcf")
+	
+	vcf_data=remove_dodgy_sites(vcf_data)
 	
 	
-	#first find the reference
-	reffound=False
-	refseq=[]
-	print "Finding reference in alignment"
-	for line in open(options.alignment, "rU"):
-		line=line.strip()
-		if len(line)>0 and line[0]==">":
-			if line.split()[0][1:]==options.reference:
-				reffound=True
-			elif reffound:
-				break
-		elif reffound:
-			refseq.append(''.join(line.split()))
-		
 	
-	if not reffound:
-		DoError("Cannot find reference in alignment")
+	bcf_handle=open_bcf(options.bcf)
+	bcf_data=parse_bcf(bcf_handle, vcf_data)
+	
+	maj_allele_identities={}
+	min_allele_identities={}
+	maj_allele_match_count=0.0
+	min_allele_match_count=0.0
+	for x in xrange(len(taxon_list)):
+		maj_allele_identities[x]=0.0
+		min_allele_identities[x]=0.0
+	
+	for chromosome in bcf_data:
+		for i in bcf_data[chromosome]:
+			if i[3]>options.count:
+				if i[1] in vcf_data[chromosome][i[0]]:
+					maj_allele_match_count+=1
+					for taxon in vcf_data[chromosome][i[0]][i[1]]:
+						maj_allele_identities[taxon]+=1
+			if float(i[4])/(i[3]+i[4])>options.proportion and i[4]>options.count:
+				if i[2] in vcf_data[chromosome][i[0]]:
+					min_allele_match_count+=1
+				#print i, float(i[4])/(i[3]+i[4])
+					for taxon in vcf_data[chromosome][i[0]][i[2]]:
+						min_allele_identities[taxon]+=1
+			#elif i[3]>options.count:
+			#	min_allele_match_count+=1
+			#	for taxon in vcf_data[chromosome][i[0]][i[1]]:
+			#		min_allele_identities[taxon]+=1
+	
+	maxmax=0
+	topmaj=""
+	topmajid=""
+	for taxon in maj_allele_identities:
+		if maj_allele_identities[taxon]>maxmax and taxon_list[taxon]!=options.id:
+			maxmax=maj_allele_identities[taxon]
+			topmaj=taxon_list[taxon]
+			topmajid=taxon
+	maxmin=0
+	topmin=""
+	topminid=""
+	for taxon in min_allele_identities:
+		if min_allele_identities[taxon]>maxmin and taxon_list[taxon]!=options.id:
+			maxmin=min_allele_identities[taxon]
+			topmin=taxon_list[taxon]
+			topminid=taxon
+	
+	
+	if maj_allele_match_count==0:
+		majval=0
 	else:
-		print "Reference found"
-	refseq=''.join(refseq).upper()
-	
-	print "Finding SNP sites"
-	SNPsites=set([])
-	seq=[]
-	
-	ok_bases=set(['A','G','C','T'])
-	
-	for line in open(options.alignment, "rU"):
-		line=line.strip()
-		if len(line)>0 and line[0]==">":
-			if line.split()[0][1:]==options.reference:
-				seq=[]
-				continue
-			seq=''.join(seq).upper()
-			if len(seq)==len(refseq):
-				for base in xrange(len(refseq)):
-					if base in SNPsites:
-						continue
-					if refseq[base]!=seq[base] and refseq[base] in ok_bases and seq[base] in ok_bases:
-						SNPsites.add(base)
+		majval=maxmax/maj_allele_match_count
 		
-			seq=[]
-		elif reffound:
-			seq.append(''.join(line.split()))
-	if line.split()[0][1:]!=options.reference:
-		seq=''.join(seq).upper()
-		if len(seq)==len(refseq):
-			for base in xrange(len(refseq)):
-				if base in SNPsites:
-					continue
-				if refseq[base]!=seq[base] and refseq[base] in ok_bases and seq[base] in ok_bases:
-					SNPsites.add(base)
-	
-	print "Found", len(SNPsites), "SNP sites"
-	
-	sorted_snps=list(SNPsites)
-	sorted_snps.sort()
-	seqnames=[]
-	
-	pairwise_distances={}
-	tot=0
-	
-	alignment={}
-	
-	refpos={}
-	
-	print "Extracting SNP sites and their positions in reference"
-	name=""
-	seq=[]
-	for line in open(options.alignment, "rU"):
-		line=line.strip()
-		if len(line)>0 and line[0]==">":
-			if name!="":
-				seqnames.append(name)
-				pairwise_distances[name]={}
-				seq=''.join(seq).upper()
-				snpseq=[]
-				for x in sorted_snps:
-					snpseq.append(seq[x])
-				
-				if name==options.reference:
-					count=0
-					SNPcount=0
-					for base in xrange(len(refseq)):
-						
-						if refseq[base]!="-":
-							count+=1
-						if base in SNPsites:
-							refpos[SNPcount]=count
-							SNPcount+=1
-
-				alignment[name]=''.join(snpseq)
-				seq=[]
-				
-			name=line.split()[0][1:]
-		elif reffound:
-			seq.append(''.join(line.split()))
-
-	if name!="":
-		seqnames.append(name)
-		pairwise_distances[name]={}
-		seq=''.join(seq).upper()
-		snpseq=[]
-		for x in sorted_snps:
-			snpseq.append(seq[x])
+	if min_allele_match_count==0:
+		minval=0
+	else:
+		minval=maxmax/min_allele_match_count
 		
-		if name==options.reference:
-			count=0
-			SNPcount=0
-			for base in xrange(len(refseq)):
-				
-				if refseq[base]!="-":
-					count+=1
-				if base in SNPsites:
-					refpos[SNPcount]=count
-					SNPcount+=1
-		alignment[name]=''.join(snpseq)
+		
+	if topmaj=="" or topmin=="":
+		majminperc="-"
+	else:
+		majminperc=percent_id[topmajid][topminid]
+		
+	if topmaj=="" or tid=="":
+		majtperc="-"
+	else:
+		majtperc=percent_id[topmajid][tid]
+	if topmin=="" or tid=="":
+		mintperc="-"
+	else:
+		mintperc=percent_id[topminid][tid]
+	
+	print ','.join(map(str, [options.id, topmaj, maxmax, majval, topmin, maxmin, minval, majtperc, mintperc, majminperc]))
+	sys.exit()
+	
+	
+		
+		
+		
 
-	
-#	print alignment
-#	print seqnames
-#	print pairwise_distances
-#	print refpos
-#	sys.exit()	
-#	
-	
-	
-#	try:
-#		readalignment=read_alignment(options.alignment)
-#	except StandardError:
-#		DoError("Cannot open alignment file")
-#
-#	seqnames=[]
-#	
-#	pairwise_distances={}
-#	tot=0
-#	
-#	alignment={}
-#	for sequence in readalignment:
-#		alignment[sequence.name]=str(sequence.seq).upper()
-#		if not sequence.name in seqnames:
-#			seqnames.append(sequence.name)
-#			pairwise_distances[sequence.name]={}
-	
-	alnlen=len(alignment[name])
-	taxacount=len(alignment)
-	seqcount=len(seqnames)
-	toremove=set([])
-	cutoff=2
-	if float(taxacount)*options.proportion>cutoff:
-		cutoff=float(taxacount)*options.proportion
-	
-	print "Cutoff set at", cutoff	
-	
-	for x in xrange(alnlen):
-		ncount=0.0
-		for seq in seqnames:
-			if alignment[seq][x] in gap_and_missing:
-				ncount+=1
-		if ncount>cutoff:
-			toremove.add(x)
 	
 	
 	
