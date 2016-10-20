e0f2ea47218246885c48fa1f51c1fb927ba48e02
Pathogen Genomics, Wellcome Trust Sanger Institute <psu-dev@sanger.ac.uk>
2016-10-20 23:00:39 +0100

    Daily update of sh16 scripts

--- chippy/extract_sequences_from_refseq.py
+++ chippy/extract_sequences_from_refseq.py
@@ -7,6 +7,10 @@ try:
 except:
 	print "Could not open file "+sys.argv[1]
 my_species=sys.argv[2]
+toprint="a"
+if len(sys.argv)>3 and sys.argv[3] in ['p','c']:
+	toprint=sys.argv[3]
+
 try:
 	idx=open(sys.argv[1]+".chippy.index", "rU")
 except:
@@ -18,6 +22,10 @@ if not my_species in index:
 	print "Could not find", my_species, "in index file"
 	sys.exit()
 for my_accession in index[my_species]:
+	if toprint=="c" and index[my_species][my_accession]['plasmid']==True:
+		continue
+	elif toprint=="p" and index[my_species][my_accession]['plasmid']==False:
+		continue
 	f.seek(index[my_species][my_accession]["locations"][0])
 	while f.tell()<index[my_species][my_accession]["locations"][1]:
 		line = f.readline()
